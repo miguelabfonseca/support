68339624615f7cd1e31fc8aa51f00946d134bebb
Miguel António <miguelabfonseca@gmail.com>
2022-09-23 18:28:32 +0100

    [UPD] db migrations

---
+++ database/seeders/tenant/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders\tenant;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10)
+        ]);
+
+    }
+}
