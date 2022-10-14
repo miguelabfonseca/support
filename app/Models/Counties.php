901dd0b624610ef75cd704c928a31a4652e0a15b
Miguel António <miguelabfonseca@gmail.com>
2022-10-14 16:52:01 +0100

    [UPG] location districts/counties

---
+++ app/Models/Counties.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Counties extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id','district_id','name','active'];
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('name');
+        });
+    }
+}
