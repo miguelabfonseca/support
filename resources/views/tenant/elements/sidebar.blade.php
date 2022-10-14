cc70a5a927d60c306704ae6f43e736783928a81a
Miguel António <miguelabfonseca@gmail.com>
2022-10-14 16:54:01 +0100

    [FIX] routes & names

--- resources/views/tenant/elements/sidebar.blade.php
+++ resources/views/tenant/elements/sidebar.blade.php
@@ -7,17 +7,16 @@
         <ul class="metismenu" id="menu">
             <li><a class="ai-icon" href="{{ route('tenant.dashboard') }}">
                     <i class="flaticon-381-networking"></i>
-                    <span class="nav-text">Dashboard</span>
+                    <span class="nav-text">{{ __('Dashboard') }}</span>
                 </a>
-
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
                     <i class="flaticon-381-windows"></i>
                     <span class="nav-text">{{ __('Manage Tasks') }}</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('tenant.tasks.list') }}">Tasks</a></li>
-                    <li><a href="{{ route('tenant.tasks.reports.list') }}">Tasks Reports</a></li>
+                    <li><a href="#">{{ __('Tasks') }}</a></li>
+                    <li><a href="#">{{ __('Reports') }}</a></li>
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
@@ -25,7 +24,7 @@
                     <span class="nav-text">{{ __('Manage Devices') }}</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('tenant.devices.list') }}">{{ __('Devices') }}</a></li>
+                    <li><a href="#">{{ __('Devices') }}</a></li>
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
@@ -33,7 +32,7 @@
                     <span class="nav-text">{{ __('Manage Services') }}</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('tenant.services.list') }}">{{ __('Services') }}</a></li>
+                    <li><a href="#">{{ __('Services') }}</a></li>
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
@@ -41,7 +40,7 @@
                     <span class="nav-text">{{ __('Manage Customers') }}</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('tenant.customers.list') }}">{{ __('Customers') }}</a></li>
+                    <li><a href="{{ route('tenant.customers.index') }}">{{ __('Customers') }}</a></li> <!-- a corrigir */ -->
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
@@ -49,15 +48,15 @@
                     <span class="nav-text">{{ __('Manage Partners') }}</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('tenant.partners.list') }}">{{ __('Partners') }}</a></li>
+                    <li><a href="#">{{ __('Partners') }}</a></li>
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
                     <i class="flaticon-381-user-9"></i>
-                    <span class="nav-text">{{ __('Team') }}</span>
+                    <span class="nav-text">{{ __('Manage Team') }}</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('tenant.team.list') }}">{{ __('List Team') }}</a></li>
+                    <li><a href="#">{{ __('Team') }}</a></li>
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
@@ -65,11 +64,11 @@
                     <span class="nav-text">{{ __('Setup') }}</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('tenant.setup.devices.list') }}">{{ __('Device Models') }}</a></li>
+                    <li><a href="#">{{ __('Device Models') }}</a></li>
                     <li><a href="{{ route('tenant.setup.brands.index') }}">{{ __('Device Brands') }}</a></li>
-                    <li><a href="{{ route('tenant.setup.parts.list') }}">{{ __('Parts') }}</a></li>
-                    <li><a href="{{ route('tenant.setup.attributes.list') }}">{{ __('Attributes') }}</a></li>
-                    <li><a href="{{ route('tenant.setup.attributesvalues.list') }}">{{ __('Attributes Values') }}</a></li>
+                    <li><a href="#">{{ __('Parts') }}</a></li>
+                    <li><a href="#">{{ __('Attributes') }}</a></li>
+                    <li><a href="#">{{ __('Attributes Values') }}</a></li>
                     <li><a href="{{ route('tenant.setup.services.index') }}">{{ __('Services')}}</a></li>
                 </ul>
             </li>
