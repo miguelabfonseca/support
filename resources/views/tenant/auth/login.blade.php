f6752053c5f6af937e40ae1353b76f3464fbb873
Miguel António <miguelabfonseca@gmail.com>
2022-10-21 17:17:34 +0100

    [UPD] views e lang

--- resources/views/tenant/auth/login.blade.php
+++ resources/views/tenant/auth/login.blade.php
@@ -6,10 +6,10 @@
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
                     <div class="auth-form">
                         <div class="text-center mb-3">
-                            <a href="{!! url('/index') !!}"><img
-                                    src="{{ 'assets/resources/images/logo-full.png' }}" alt=""></a>
+                            <a href="{!! url('/') !!}"><img
+                                    src="http://moto.brvr/assets/resources/images/logo-text.png" alt=""></a>
                         </div>
-                        <h4 class="text-center mb-4 text-white">Sign in your account</h4>
+                        <h4 class="text-center mb-4 text-white">Acesso Proensal</h4>
                         <form method="POST" action="{{ route('tenant.verify') }}">
                             @csrf
                             <div class="form-group">
@@ -18,34 +18,34 @@
                                     :value="old('email')" required autofocus />
                             </div>
                             <div class="form-group">
-                                <x-label for="password" class="mb-1 text-white" :value="__('Password')" />
+                                <x-label for="password" class="mb-1 text-white" :value="__('Senha')" />
                                 <x-input id="password" class="block mt-1 w-full form-control" type="password"
                                     name="password" required autocomplete="current-password" />
                             </div>
                             <div class="form-row d-flex justify-content-between mt-4 mb-2">
-                                <div class="form-group">
+                                <div class="form-group d-none">
                                     <div class="custom-control custom-checkbox ml-1 text-white">
                                         <label for="remember_me" class="inline-flex items-center">
                                             <input id="remember_me" type="checkbox"
                                                 class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                                 name="remember">
-                                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                            <span class="ml-2 text-sm text-gray-600">{{ __('Lembrar-') }}</span>
                                         </label>
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <a class="text-white" href="{!! url('/forgot-password') !!}">{{ __('Forgot Password?') }}</a>
+                                    <a class="text-white" href="{!! url('/forgot-password') !!}">{{ __('Perdeu a senha?') }}</a>
                                 </div>
                             </div>
                             <div class="text-center">
                                 <button type="submit"
-                                    class="btn bg-white text-primary btn-block">{{ __('Sign Me In') }}</button>
+                                    class="btn bg-white text-primary btn-block">{{ __('Entrar') }}</button>
                             </div>
                         </form>
-                        <div class="new-account mt-3">
+                        {{-- <div class="new-account mt-3">
                             <p class="text-white">{{ __('Don\'t have an account?') }} <a class="text-white"
                                     href="{!! url('/sign-up') !!}">{{ __('Sign up') }}</a></p>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
